39575c455618eda5e8b3a98dbb76361616adc925
ezrankayamba <ezrankayamba@gmail.com>
2020-07-27 18:02:36 +0300

    Added nature imports

--- backend_rest/complaints/admin.py
+++ backend_rest/complaints/admin.py
@@ -1,6 +1,39 @@
 from django.contrib import admin
 from . import models
+from . import forms
+from django.urls import path
+from django.shortcuts import render, redirect
+import csv
+import io
 
-
-admin.site.register(models.Nature)
+# admin.site.register(models.Nature)
 admin.site.register(models.Complaint)
+
+
+@admin.register(models.Nature)
+class NatureAdmin(admin.ModelAdmin):
+    change_list_template = "complaints/natures_changelist.html"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('import-csv/', self.import_csv),
+        ]
+        return my_urls + urls
+
+    def import_csv(self, request):
+        if request.method == "POST":
+            csv_file = request.FILES["csv_file"]
+            reader = csv.DictReader(io.StringIO(csv_file.read().decode()))
+            print("Importing...", reader)
+            for row in reader:
+                name = row['name']
+                nature = models.Nature.objects.filter(name=name).first()
+                if not nature:
+                    nature = models.Nature.objects.create(name=name)
+                print(name)
+            self.message_user(request, "Your csv file has been imported")
+            return redirect("..")
+        form = forms.CsvImportForm()
+        payload = {"form": form}
+        return render(request, "complaints/csv_form.html", payload)
--- backend_rest/complaints/forms.py
+++ backend_rest/complaints/forms.py
@@ -6,3 +6,7 @@ class ComplaintForm(forms.ModelForm):
     class Meta:
         model = models.Complaint
         fields = '__all__'
+
+
+class CsvImportForm(forms.Form):
+    csv_file = forms.FileField()
